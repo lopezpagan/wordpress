d4b7680f6314e5be4734ba4acd8ca2f95c1ae1f9
Tony López <tony@lopezpagan.com>
2016-12-31 01:51:58 -0400

    Some demo pages

--- wptheme/config.rb
+++ wptheme/config.rb
@@ -16,7 +16,7 @@ javascripts_dir = "assets/js"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-#line_comments = true
+line_comments = true
 
 preferred_syntax = :scss
 
